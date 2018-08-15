713939b0f4c04ce4e1cff4261145ab9dcc30d475
Fatih Tasdemir <tsdmrfth@gmail.com>
2018-08-15 03:36:13 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component {
+
+    render(){
+        return(
+            <View>
+                <Text>
+                    Application
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default App;
